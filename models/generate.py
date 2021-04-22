f36bacc3307120733bd11856bafab419799294f4
AshwinParanjape <ashwing.2005@gmail.com>
2021-04-22 09:43:52 -0700

    Fixed batch_size arg type

--- models/generate.py
+++ models/generate.py
@@ -98,7 +98,7 @@ def generate():
                                      help="Sample from top_k docs (Marginalized, ELBO)")
     decoding_group.add_argument('--docs_sampling_temperature', type=float, default=1,
                                      help="Temperature used for sampling docs (Marginalized, ELBO)")
-    decoding_group.add_argument('--batch_size', type=float, default=1,
+    decoding_group.add_argument('--batch_size', type=int, default=1,
                                 help="Number of source strings used at a time")
 
     Experiment.add_argument_group(parser)
