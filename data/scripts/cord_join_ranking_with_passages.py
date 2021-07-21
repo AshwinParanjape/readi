61cda4ec36c777d4cd61639be457bafbf1c86bd0
Ashwin Paranjape <ashwing.2005@gmail.com>
2021-07-21 00:36:58 +0000

    Separated join_ranking_with_passages script for cord dataset

--- data/scripts/cord_join_ranking_with_passages.py
+++ data/scripts/cord_join_ranking_with_passages.py
@@ -4,17 +4,11 @@ import gzip
 from tqdm import tqdm
 import csv
 
-##rankings_path = '/u/scr/ashwinp/research/readi/experiments/colbert-rerank/15/ranking.tsv'
-#rankings_path = '/scr/biggest/ashwinp/experiments/colbert-rerank/73/ranking.tsv'
-##output_path = '/u/scr/ashwinp/research/readi/experiments/colbert-rerank/15/ranking_passages.tsv'
-#output_path = '/scr/biggest/ashwinp/experiments/colbert-rerank/73/ranking_passages.tsv'
-##passages_path = '/u/scr/ashwinp/research/readi/data/dpr-wiki/psgs_w100.tsv.gz'
-##passages_path = '/scr/biggest/ashwinp/readi/data/dpr-wiki/psgs_w100.tsv.gz'
-passages_path = '/scr/biggest/ashwinp/readi/data/kilt/kilt_ks_chunks.tsv'
 
 for expid in ['120', '121', '122', '123']:
     rankings_path = f'/scr/biggest/ashwinp/experiments/colbert-rerank/{expid}/ranking.tsv'
     output_path = f'/scr/biggest/ashwinp/experiments/colbert-rerank/{expid}/ranking_passages.tsv'
+    passages_path = '/scr/biggest/ashwinp/readi/data/CORDv1/corpus/collection.tsv'
     rankings_df = pd.read_csv(rankings_path, sep='\t', names=['qid', 'pid', 'rank', 'score'])
     joined_rankings_dfs = []
 
@@ -25,14 +19,13 @@ for expid in ['120', '121', '122', '123']:
         fileh = open(passages_path)
 
     with fileh:
-        for chunk_df in tqdm(pd.read_csv(fileh, sep='\t', chunksize=1000000, names=['pid', 'text', 'title'], header=0, usecols=['pid', 'text', 'title'], quoting=csv.QUOTE_NONE, doublequote=False, lineterminator='\n', quotechar=None)):
+        for chunk_df in tqdm(pd.read_csv(fileh, sep='\t', chunksize=1000000, names=['pid', 'text', 'title', 'cid'], header=0, usecols=['pid', 'text', 'title', 'cid'], quoting=csv.QUOTE_NONE, doublequote=False, lineterminator='\n', quotechar=None)):
             chunk_df.set_index('pid', inplace=True)
             joined_rankings_dfs.append(rankings_df.join(chunk_df, on='pid', how='inner'))
 
 
     joined_rankings_df = pd.concat(joined_rankings_dfs).sort_values(by=['qid', 'rank'])
     joined_rankings_df.to_csv(output_path, sep='\t', quoting=csv.QUOTE_NONE, doublequote=False, line_terminator='\n', quotechar=None, index=False)
-#joined_rankings_df.to_csv(output_path, sep='\t', index=False)
 
 
 
