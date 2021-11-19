6a9b610be30577a4fa613cb2bec14da97b6783a4
AshwinParanjape <ashwing.2005@gmail.com>
2021-11-19 08:05:54 -0800

    Modified pkl to txt script for no retrieval generation

--- scripts/generations_pkl_to_txt.py
+++ scripts/generations_pkl_to_txt.py
@@ -14,14 +14,23 @@ with open(txt_path, 'w') as fout:
     for gen in gens:
         print('Conv:', ' | '.join(gen['source'].split('|')[-2:]), file=fout)
         print('target:', gen['target'], file=fout)
-        for ret in gen['retrievals']:
-            print('\t Doc:', ret['doc_score'], ret['doc_text'], file=fout)
-            if 'generator_output' in ret:
-                if 'log_liklihood' in ret['generator_output'] and 'baseline_log_liklihood' in ret['generator_output']:
-                    for s, ll, bll in zip(ret['generator_output']['strings'], ret['generator_output']['log_liklihood'], ret['generator_output']['baseline_log_liklihood']):
-                        print('\t\t Gen output:', ll, bll, s, file=fout)
-                else:
-                    for s in zip(ret['generator_output']['strings']):
-                        print('\t\t Gen output:', s, file=fout)
-                print('-'*100, file=fout)
+        if 'retrievals' in gen:
+            for ret in gen['retrievals']:
+                print('\t Doc:', ret['doc_score'], ret['doc_text'], file=fout)
+                if 'generator_output' in ret:
+                    if 'log_liklihood' in ret['generator_output'] and 'baseline_log_liklihood' in ret['generator_output']:
+                        for s, ll, bll in zip(ret['generator_output']['strings'], ret['generator_output']['log_liklihood'], ret['generator_output']['baseline_log_liklihood']):
+                            print('\t\t Gen output:', ll, bll, s, file=fout)
+                    else:
+                        for s in zip(ret['generator_output']['strings']):
+                            print('\t\t Gen output:', s, file=fout)
+                    print('-'*100, file=fout)
+        else:
+            if 'log_liklihood' in gen['generator_output'] and 'baseline_log_liklihood' in gen['generator_output']:
+                for s, ll, bll in zip(gen['generator_output']['strings'], gen['generator_output']['log_liklihood'], gen['generator_output']['baseline_log_liklihood']):
+                    print('\t\t Gen output:', ll, bll, s, file=fout)
+            else:
+                for s in zip(gen['generator_output']['strings']):
+                    print('\t\t Gen output:', s, file=fout)
+            print('-'*100, file=fout)
         print('='*100, file=fout)
